af2945c4e54715de9b92db8ae009867d18cc547a
acedia <87400916+acedia-a@users.noreply.github.com>
2026-07-29 01:04:05 +0700

    successful in moving places with interactable door
    text still doesnt appear

--- Global/SceneManager.gd
+++ Global/SceneManager.gd
@@ -10,10 +10,8 @@ var maps := {
 	"Classroom": "res://Scenes/Maps/Classroom.tscn"
 }
 
-
 func register_player(p: CharacterBody2D):
 	player = p
-
 
 func change_map(map_name: String, spawn_name: String):
 
@@ -21,21 +19,29 @@ func change_map(map_name: String, spawn_name: String):
 		push_error("Map tidak ditemukan: " + map_name)
 		return
 
-	var scene := load(maps[map_name]) as PackedScene
+	var main = get_tree().current_scene
 
+	# Selamatkan player sebelum map lama dihapus
+	if player.get_parent():
+		player.reparent(main)
+
+	# Hapus map lama
 	if current_map:
 		current_map.queue_free()
+		await get_tree().process_frame
 
+	# Load map baru
+	var scene: PackedScene = load(maps[map_name])
 	current_map = scene.instantiate()
-
-	var main = get_tree().current_scene
 
 	main.get_node("CurrentMap").add_child(current_map)
 
 	await get_tree().process_frame
 
+	# Cari spawn
 	var spawn = current_map.get_node("YSort/SpawnPoints/" + spawn_name)
-	
+
+	# Masukkan player ke map baru
 	player.reparent(current_map.get_node("YSort"))
 
 	player.global_position = spawn.global_position
--- Scripts/Objects/doors.gd
+++ Scripts/Objects/doors.gd
@@ -3,5 +3,8 @@ extends Interactable
 @export var target_map : String
 @export var target_spawn : String
 
+func _ready():
+	add_to_group("doors")
+	
 func interact() -> void:
 	SceneManager.change_map(target_map, target_spawn)
