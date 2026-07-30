0b8109041b5c2e7770a2f50db18a8a3b4eea2419
acedia <87400916+acedia-a@users.noreply.github.com>
2026-07-31 00:00:19 +0700

    adding text to interactable (draft)

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,7 +2,15 @@ extends CanvasLayer
 
 @onready var time_label = $MarginContainer/VBoxContainer/TimeLabel
 
+@onready var prompt = $PromptLabel
 
+func show_prompt(text := "Tekan E"):
+	prompt.text = text
+	prompt.visible = true
+
+func hide_prompt():
+	prompt.visible = false
+	
 func _ready():
 
 	TimeManager.minute_changed.connect(update_time)
